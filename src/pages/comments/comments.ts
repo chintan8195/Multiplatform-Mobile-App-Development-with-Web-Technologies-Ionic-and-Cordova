dae3fddbe24d69e927ea1e59fc5ed68ec080336a
Chintan Parikh <cparikh@scu.edu>
2018-09-03 12:34:53 -0700

    Ionic Alerts Toast and Loading

---
+++ src/pages/comments/comments.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController,NavParams, ViewController } from 'ionic-angular';
+import {Validators, FormBuilder, FormGroup } from '@angular/forms';
+
+
+/**
+ * Generated class for the CommentsPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-comments',
+  templateUrl: 'comments.html',
+})
+export class CommentsPage {
+
+  addComment : FormGroup;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    public viewCtrl: ViewController,  private formBuilder: FormBuilder ) {
+
+      this.addComment = this.formBuilder.group({
+        author:['',Validators.required],
+        rating:5,
+        comment: ['', Validators.required],
+      });
+  }
+  
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad CommentsPage');
+  }
+
+  dismiss() {
+    this.viewCtrl.dismiss();
+  }
+
+  onSubmit() {
+    console.log(this.addComment.value);
+    this.viewCtrl.dismiss();
+  }
+
+}
--- src/pages/dishdetail/dishdetail.ts
+++ src/pages/dishdetail/dishdetail.ts
@@ -1,7 +1,10 @@
 import { Component, Inject } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController,ActionSheetController, ViewController } from 'ionic-angular';
 import { Dish } from '../../shared/dish';
 import { FavoriteProvider } from '../../providers/favorite/favorite';
+import { ModalController } from 'ionic-angular';
+
+import {CommentsPage} from '../../pages/comments/comments'
 
 
 /**
@@ -24,11 +27,17 @@ export class DishdetailPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
     @Inject('BaseURL') private BaseURL,
-    private favoriteservice: FavoriteProvider) {
+    private favoriteservice: FavoriteProvider,
+    private toastCtrl: ToastController,
+    private actionSheetCtrl: ActionSheetController,
+    public modalCtrl: ModalController,
+    public viewCtrl: ViewController) {
     this.dish = navParams.get('dish');
     this.favorite = favoriteservice.isFavorite(this.dish.id);
 
+    this.dish = navParams.get('dish');
     this.numcomments = this.dish.comments.length;
+
     let total = 0;
     this.dish.comments.forEach(comment => total += comment.rating );
     this.avgstars = (total/this.numcomments).toFixed(2);
@@ -40,6 +49,45 @@ export class DishdetailPage {
   addToFavorites() {
     console.log('Adding to Favorites', this.dish.id);
     this.favorite = this.favoriteservice.addFavorite(this.dish.id);
+    this.toastCtrl.create({
+      message: 'Dish ' + this.dish.id + ' added as favorite successfully',
+      position: 'middle',
+      duration: 3000}).present();
   }
+
+  openComments() {
+
+    let modal = this.modalCtrl.create(CommentsPage);
+    modal.present();
+  }
+
+  presentActionSheet(){
+  const actionSheet = this.actionSheetCtrl.create({
+    title: 'Select Actions',
+    buttons: [
+      {
+        text: 'Add to favorites',
+        handler: () => {
+          this.addToFavorites();
+        }
+      },
+      {
+        text: 'Add Comment',
+        handler: () => {
+         this.openComments();
+        }
+      },
+      {
+        text: 'Cancel',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }
+    ]
+  });
+actionSheet.present();
+  
+}
 
 }
